d5e168f39d42c6890cf56eb9c47c7f3341bd7882
Nauti-Rohit <rohitnautiyal9@gmail.com>
2024-05-26 17:02:30 +0530

    issue solve

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import footer_logo from "../../assets/footer_logo.svg";
+import footer_logo from "../../assets/logo.svg";
 import user_icon from "../../assets/user_icon.svg";
 const Footer = () => {
   return (
